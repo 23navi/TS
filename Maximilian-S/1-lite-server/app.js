55481d32d4e090bb7db70679603f841d13802fa3
Navi sureka <navisureka23@gmail.com>
2023-03-21 22:09:08 +0530

    lite-server config

--- Maximilian-S/1-lite-server/app.js
+++ Maximilian-S/1-lite-server/app.js
@@ -1,3 +1,3 @@
 // install and config the package.json to use lite-server 
 // npm i lite-server
-
+console.log("Navi Sureka is the best");
---
+++ Maximilian-S/1-lite-server/app.ts
@@ -0,0 +1,6 @@
+// install and config the package.json to use lite-server  => "start":"lite-server"
+// npm i lite-server
+// bs-config.json tells lite-server what file should be served at start ie: / path in url
+
+console.log("Navi Sureka is the best")
+
