9007ffe0bd296e97ce21099a3cb62d2943522e40
Navi sureka <navisureka23@gmail.com>
2023-03-21 22:28:35 +0530

    got it

--- Maximilian-S/1-lite-server/app.js
+++ Maximilian-S/1-lite-server/app.js
@@ -1,3 +1,13 @@
-// install and config the package.json to use lite-server 
+// install and config the package.json to use lite-server  => "start":"lite-server"
 // npm i lite-server
+// bs-config.json tells lite-server what file should be served at start ie: / path in url
 console.log("Navi Sureka is the best");
+//number casting from string to number
+var a = 5;
+var b = "6";
+console.log(typeof Number);
+function abc(a, b) {
+    console.log(a + b);
+    console.log(a.toString());
+}
+abc(a, +b);
--- Maximilian-S/1-lite-server/app.ts
+++ Maximilian-S/1-lite-server/app.ts
@@ -2,5 +2,18 @@
 // npm i lite-server
 // bs-config.json tells lite-server what file should be served at start ie: / path in url
 
-console.log("Navi Sureka is the best")
+console.log("Navi Sureka is the best");
 
+//number casting from string to number
+
+let a = 5;
+let b = "6";
+
+console.log(typeof Number);
+
+function abc(a: number, b: number) {
+  console.log(a + b);
+  console.log(a.toString());
+}
+
+abc(a, +b);
