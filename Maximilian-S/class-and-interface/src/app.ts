c8d3e27fededd6200debf7d60c55198de51e1601
Navi sureka <navisureka23@gmail.com>
2023-03-22 22:47:29 +0530

    readonly

--- Maximilian-S/class-and-interface/src/app.ts
+++ Maximilian-S/class-and-interface/src/app.ts
@@ -1,1 +1,22 @@
 // Code goes here!
+
+class Navi {
+  readonly name: string;
+
+  constructor() {
+    this.name = "Navi";
+  }
+
+  
+}
+
+// constructor shorthand
+// class Department {
+//   constructor(public name: string, private age: number) {}
+// }
+
+// let b = new Department("Navi", 34);
+// console.log(b.name);
+
+let a = new Navi();
+console.log(a.name);
