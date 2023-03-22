21f36f7d884814bba11f7f66ec843d235500a687
Navi sureka <navisureka23@gmail.com>
2023-03-22 19:02:31 +0530

    added drag-drop stater coce

--- Stephen-Grider/Basics/enum.js
+++
@@ -1,8 +0,0 @@
-var MyEnum;
-(function (MyEnum) {
-    MyEnum[MyEnum["no"] = 0] = "no";
-    MyEnum[MyEnum["yes"] = 1] = "yes";
-    MyEnum[MyEnum["maybe"] = 2] = "maybe";
-})(MyEnum || (MyEnum = {}));
-var navi = MyEnum.no;
-console.log(navi);
--- Stephen-Grider/Objects/destructuring.ts
+++ Stephen-Grider/Objects/destructuring.ts
@@ -2,16 +2,17 @@ const book = {
   title: "abc",
   author: "Navi",
   rating: 5,
-  sales:{
-    india:100,
-    abroad:400
-  }
+  sales: {
+    india: 100,
+    abroad: 400,
+  },
 };
 
-const displayBook = ({title,author}: { title: string; author: string }) => {
+const displayBook = ({ title, author }: { title: string; author: string }) => {
   console.log(author);
 };
 
-
-const {sales:{india,abroad}}:{sales:{india:number,abroad:number}}=book
+const {
+  sales: { india, abroad },
+}: { sales: { india: number; abroad: number } } = book;
 displayBook(book);
