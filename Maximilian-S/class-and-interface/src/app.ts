1f95c7e5393698d77acf4c90102062e30141cbe7
Navi sureka <navisureka23@gmail.com>
2023-03-23 17:41:28 +0530

    class

--- Maximilian-S/class-and-interface/src/app.ts
+++ Maximilian-S/class-and-interface/src/app.ts
@@ -2,9 +2,11 @@
 
 class Navi {
   readonly name: string;
+  static hey="Navi";
 
   constructor() {
     this.name = "Navi";
+    console.log(Navi.hey);
   }
 
   
@@ -18,5 +20,29 @@ class Navi {
 // let b = new Department("Navi", 34);
 // console.log(b.name);
 
-let a = new Navi();
-console.log(a.name);
+// let a = new Navi();
+// console.log(a.name);
+
+
+
+// class Boy{
+//   static  gender="Male";
+//   name:string;
+  
+
+//   constructor(){
+//     this.name="Navi";
+//     Boy.gender="yo";
+//   }
+
+//   static isBoy(){
+//     this.name="boy"
+//     console.log("hello");
+//     Boy.gender="hello";
+//   }
+// }
+
+// console.log(Boy.gender);
+
+
+
