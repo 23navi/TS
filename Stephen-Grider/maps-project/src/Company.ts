1c9ad6d58834fcc5c0a4815492b45cbe1585d501
Navi sureka <navisureka23@gmail.com>
2023-03-30 22:04:19 +0530

    added company class

--- Stephen-Grider/maps-project/src/Company.ts
+++ Stephen-Grider/maps-project/src/Company.ts
@@ -1,1 +1,17 @@
-export class Company {}
+import { faker } from "@faker-js/faker";
+export class Company {
+    companyName:string;
+    catchPhrase:string;
+    location: {
+        lat: number;
+        long: number;
+    }
+    constructor(){
+        this.companyName=faker.company.name();
+        this.catchPhrase=faker.company.catchPhrase();
+        this.location = {
+            lat: +faker.address.latitude,
+            long: +faker.address.longitude,
+          };
+    } 
+}
