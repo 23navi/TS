22113b0474af3e888a647a4d82da82c3d554989d
Navi sureka <navisureka23@gmail.com>
2023-04-17 16:52:14 +0530

    sorting-project completed

--- Stephen-Grider/sorting-project/src/index.ts
+++ Stephen-Grider/sorting-project/src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello TS");
+import { CharactersCollections } from "./CharactersCollection";
+import {NumbersCollections} from "./NumbersCollection"
+
+
+const stringData="Naviiii";
+const numberArr=[3,5,6,7,1,2,34,64,22,34,53,13,52,53,63,1,3,5,6,7,8,9]
+
+const numberCol= new NumbersCollections(numberArr);
+numberCol.sort();
+console.log(numberCol);
+
+const charCol= new CharactersCollections(stringData);
+charCol.sort();
+console.log(charCol);
+
+
