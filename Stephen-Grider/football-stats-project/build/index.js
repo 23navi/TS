4559cebca561b71068ada85f5318606672da8adf
Navi sureka <navisureka23@gmail.com>
2023-04-21 23:06:09 +0530

    update

--- Stephen-Grider/football-stats-project/build/index.js
+++ Stephen-Grider/football-stats-project/build/index.js
@@ -1,2 +1,20 @@
 "use strict";
-console.log("Hello football-stats");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+//our matches is string[][]
+const matches = fs_1.default.readFileSync("football.csv", { encoding: "utf-8" }).split("\n").map((row) => {
+    return row.split(",");
+});
+let manWon = 0;
+for (let match of matches) {
+    if (match[1] === "Man United" && match[5] === "H") {
+        manWon++;
+    }
+    else if (match[2] === "Man United" && match[5] === "A") {
+        manWon++;
+    }
+}
+console.log(manWon);
