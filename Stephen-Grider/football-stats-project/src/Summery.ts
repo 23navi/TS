bb8a7c1d638dfe607d6b8e17705a562897af400b
23navi <navisureka23@gmail.com>
2023-08-16 18:12:04 +0530

    Summery.ts

---
+++ Stephen-Grider/football-stats-project/src/Summery.ts
@@ -0,0 +1,18 @@
+import { MatchData } from "./inheritance (abstract class)/MatchData.type";
+
+export interface IAnalyzer {
+  run(match: MatchData[]): string;
+}
+export interface IOutputTarget {
+  //Report is just the string output from Analyzer
+  print(report: string): void;
+}
+
+export class Summary {
+  constructor(public analyzer: IAnalyzer, public outputTarget: IOutputTarget) {}
+
+  buildAndPrintReport(matches: MatchData[]): void {
+    const report = this.analyzer.run(matches);
+    this.outputTarget.print(report);
+  }
+}
