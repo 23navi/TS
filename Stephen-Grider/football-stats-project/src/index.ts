d0eae74fcf7ada040e53551648edee942346249c
23navi <navisureka23@gmail.com>
2023-08-16 19:50:05 +0530

    static on Summary

--- Stephen-Grider/football-stats-project/src/index.ts
+++ Stephen-Grider/football-stats-project/src/index.ts
@@ -1,9 +1,6 @@
 import { CsvFileReader } from "./composition (interface)/CsvFileReader";
 import { MatchFileReader } from "./composition (interface)/MatchReader";
 import { Summary } from "./Summary";
-import { WinsAnalysis } from "./analysis/matchWonAnalysis";
-import { ConsoleReport } from "./outputReports/consoleReport";
-import { HtmlReport } from "./outputReports/htmlReport";
 
 // const matchFileReader = new MatchFileReader("football.csv");
 // matchFileReader.read();
@@ -15,9 +12,6 @@ matchReader.load();
 
 const matches = matchReader.matches;
 
-const summary = new Summary(
-  new WinsAnalysis("Man United"),
-  new HtmlReport()
-);
+const summary = Summary.WinAnalaysisWithHtmlReport("Man United");
 
 summary.buildAndPrintReport(matches);
