9c06fec46f16d9c2009b2c52bd708293a87f3e18
23navi <navisureka23@gmail.com>
2023-09-16 11:22:19 +0530

    authSystem init

---
+++ Stephen-Grider/Express/AuthSystem/src/routers/loginRouter.ts
@@ -0,0 +1,11 @@
+import {Router,Request,Response} from "express";
+
+const router= Router();
+
+
+router.get("/",(req:Request,res:Response)=>{
+    res.send('<h1>Hello</h1>')
+})
+
+
+export {router};
