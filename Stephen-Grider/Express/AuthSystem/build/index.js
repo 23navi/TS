6b123ad471d714ea6a8fdfaf3529c62a84fef2c6
23navi <navisureka23@gmail.com>
2023-09-16 11:31:48 +0530

    skaffold for authService

---
+++ Stephen-Grider/Express/AuthSystem/build/index.js
@@ -0,0 +1,12 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const loginRouter_1 = require("./routers/loginRouter");
+const app = (0, express_1.default)();
+app.use(loginRouter_1.router);
+app.listen(3001, () => {
+    console.log("Listening to port 3001");
+});
