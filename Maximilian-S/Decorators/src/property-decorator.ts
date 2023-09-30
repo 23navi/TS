eb4a164f2e1c509908336cd3883d2d1d1f7b808c
23navi <navisureka23@gmail.com>
2023-09-30 09:18:55 +0530

    method decorator

--- Maximilian-S/Decorators/src/property-decorator.ts
+++ Maximilian-S/Decorators/src/property-decorator.ts
@@ -3,11 +3,21 @@ function InstancePropertyDecorator(target: any, name: string) {
   console.log({ name });
 }
 
+function MethodPropertyDecorator(
+  target: any,
+  name: string,
+  descriptor: PropertyDescriptor
+) {
+  console.log({ target });
+  console.log({ name });
+  console.log({ descriptor });
+}
+
 class Person1 {
-  @InstancePropertyDecorator
+  //   @InstancePropertyDecorator
   static lastName = "abc";
 
-  @InstancePropertyDecorator
+  //   @InstancePropertyDecorator
   name: string;
   private _age: number = 0;
   constructor(name: string) {
@@ -19,9 +29,11 @@ class Person1 {
   get age() {
     return this._age;
   }
+  @MethodPropertyDecorator
   getNextAge() {
     return this._age + 1;
   }
+  @MethodPropertyDecorator
   static getDefaultAge() {
     // Can't access this._age from a static method
     return 0;
