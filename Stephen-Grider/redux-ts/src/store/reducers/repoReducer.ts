6f7700368a2a96106a658bdefcb71413c2574224
23navi <navisureka23@gmail.com>
2023-11-19 10:40:51 +0530

    action enum and interfaces

--- Stephen-Grider/redux-ts/src/store/reducers/repoReducer.ts
+++ Stephen-Grider/redux-ts/src/store/reducers/repoReducer.ts
@@ -3,14 +3,37 @@ interface IRepoState {
   error: null | string;
   loading: boolean;
 }
-const reducer = (state: IRepoState, action: any): IRepoState => {
-  switch (action.types) {
-    case "action_type_name success":
+
+enum ActionTypes {
+  search_repo_loading,
+  search_repo_success,
+  search_repo_error,
+}
+
+interface ISearchRepo {
+  type: ActionTypes.search_repo_loading;
+}
+
+interface ISearchRepoSuccess {
+  type: ActionTypes.search_repo_success;
+  payload: string[];
+}
+
+interface ISearchRepoError {
+  type: ActionTypes.search_repo_error;
+  payload: string;
+}
+
+type IAction = ISearchRepo | ISearchRepoSuccess | ISearchRepoError;
+
+const reducer = (state: IRepoState, action: IAction): IRepoState => {
+  switch (action.type) {
+    case ActionTypes.search_repo_success:
       return { loading: false, error: null, data: action.payload };
-    case "action_type_name error":
+    case ActionTypes.search_repo_error:
       return { loading: false, error: "error", data: [] };
 
-    case "action_type_name loading":
+    case ActionTypes.search_repo_loading:
       return { loading: true, error: null, data: [] };
     default:
       return state;
